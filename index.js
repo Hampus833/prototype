c9058e76e850d178a55cb03d254bbc5a3460f808
Hampus833 <hampuse266@gmail.com>
2026-04-04 04:11:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 //bestämma när användaren ska levla up och relevant kod för det
 //Fixa buggen i leaderboard 
 //skriva korrekt descirptions för alla badges, och hur de låses upp.
+//fixa så badges låses upp vid korrekta tidpunkter
 
 renderHeader(document.querySelector("body"));
 // renderStart();
