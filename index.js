42e4f2946106a0e77e55f65aa4ef8414af6bbb11
Hampus833 <hampuse266@gmail.com>
2026-04-05 00:35:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //todo
-//fixa tillbaka css så att contentet ligger närmre mitten av skärmen
 //bestämma när användaren ska levla up och relevant kod för det
 //skriva korrekt descirptions för alla badges, och hur de låses upp.
 //fixa så badges låses upp vid korrekta tidpunkter
 //växla mellan frågorna
+//level, nickname och avatar ska inte finnas förrän man klickat på start
 
 renderHeader(document.querySelector("body"));
 // renderStart();
