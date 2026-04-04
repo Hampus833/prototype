00e2bd49de871319d41af7f3177e30ddafa5f3dd
Hampus833 <hampuse266@gmail.com>
2026-04-04 23:53:22 +0200

    edited css

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 //todo
 //fixa tillbaka css så att contentet ligger närmre mitten av skärmen
 //bestämma när användaren ska levla up och relevant kod för det
-//Fixa buggen i leaderboard 
 //skriva korrekt descirptions för alla badges, och hur de låses upp.
 //fixa så badges låses upp vid korrekta tidpunkter
-//fixa en default avatar och inte bara en rosa cirkel som det är nu
 
 renderHeader(document.querySelector("body"));
-renderStart();
-// renderQuiz();
+// renderStart();
 renderFooter();
+renderQuiz();
 // renderCodePage();
 // renderLeaderboard();
 // renderLevelUp();
--- state/state.js
+++ state/state.js
@@ -1,7 +1,7 @@
 let appState = {
     nickname: "Nickname",
     points: 0,
-    question: 3,
+    question: 1,
     level: 1,
 }
 
